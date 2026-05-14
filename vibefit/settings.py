6fea2aa26a1ec0e22a0c425d4d25b7656efaea95
burcinsari <burcin.sari@std.yeditepe.edu.tr>
2026-05-14 15:26:19 +0300

    eski hale döndürüldü

--- vibefit/settings.py
+++ vibefit/settings.py
@@ -1,5 +1,4 @@
-import os
-import dj_database_url
+
 """
 Django settings for vibefit project.
 
@@ -78,10 +77,10 @@ WSGI_APPLICATION = 'vibefit.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    
-        'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     
 }
 
